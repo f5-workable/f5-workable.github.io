c5ae1cb881e6ca9a864f57d397626cabec191c32
yelim1215 <yerim1215jg@naver.com>
2023-05-02 16:23:08 +0900

    초기설정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
