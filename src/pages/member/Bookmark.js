ec2fd5859e0947e7b54dd954129b43b58f77d7cb
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-08 02:34:58 +0900

    북마크 페이지 디자인

---
+++ src/pages/member/Bookmark.js
@@ -0,0 +1,16 @@
+import SearchedItems from "../../components/member/SearchedItems";
+
+const Bookmark = () => {
+  return (
+    <main className="w-full h-full pt-14 pb-10 ">
+      <div className="w-11/12 lg:w-3/4 h-auto min-h-full mx-auto">
+        <div className="mb-8">
+          <h2 className="text-2xl font-semibold text-neutral-800">북마크</h2>
+        </div>
+        <SearchedItems />
+      </div>
+    </main>
+  );
+};
+
+export default Bookmark;
