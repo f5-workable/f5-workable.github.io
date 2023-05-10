9792def9a9fc19f6b4a786363f9a1acfab4c3f92
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-10 16:37:47 +0900

    id 속성 값 변경, name 속성 값 추가

--- src/components/member/searchDetail/deadline/index.js
+++ src/components/member/searchDetail/deadline/index.js
@@ -5,14 +5,16 @@ const Deadline = () => {
       <div className="flex items-center">
         <div className="w-96 p-2 bg-white rounded-3xl flex justify-center items-center mr-5 border border-solid border-neutral-400">
           <input
-            id="date-start"
+            id="deadline-start"
+            name="deadline-start"
             type="date"
             defaultValue={new Date().toISOString().split("T")[0]}
             className="p-1"
           />
           <span className="mx-4">~</span>
           <input
-            id="date-end"
+            id="deadline-end"
+            name="deadline-end"
             type="date"
             defaultValue={new Date().toISOString().split("T")[0]}
             className="p-1"
