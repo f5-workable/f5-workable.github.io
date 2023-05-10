e64cc370cb555479e258f5ee8777a11c7537d714
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-10 13:00:30 +0900

    기업쪽 슬라이더 컴포넌트 생성

---
+++ src/components/company/slider/HeadItem.js
@@ -0,0 +1,13 @@
+const HeadItem = () => {
+  return (
+    <div className="w-full h-full rounded-lg overflow-hidden">
+      <img
+        className="w-full h-80 object-cover"
+        src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fbanners%2F1975%2Fdebe424c.jpg&w=1060&q=100"
+        alt="slider img"
+      />
+    </div>
+  );
+};
+
+export default HeadItem;
