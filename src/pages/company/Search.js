d2a0b103eb26429727ad14a0b3040b89d1859bc1
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-11 14:29:25 +0900

    기업쪽 상세 검색 페이지 디자인

---
+++ src/pages/company/Search.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import SearchDetail from "../../components/company/searchDetail";
+import SearchedItems from "../../components/company/searchedItems";
+
+const Search = () => {
+  const [sortBy, setSortBy] = useState("");
+  const ChangeSortBy = (e) => setSortBy(e.target.value);
+
+  return (
+    <main className="w-full h-full py-10">
+    <SearchDetail />
+    <div className="w-11/12 lg:w-3/4 h-auto mx-auto">
+      <div className="flex justify-between items-center mb-8">
+        <h2 className="text-2xl font-bold">검색결과 - 10개</h2>
+        <select
+          className="text-lg px-3 py-2 border border-solid border-neutral-300 rounded-lg text-neutral-600"
+          onChange={ChangeSortBy}
+        >
+          <option value="최신순">최신순</option>
+          <option value="임금순">임금순</option>
+        </select>
+      </div>
+      <SearchedItems sortBy={sortBy} />
+    </div>
+  </main>
+  );
+};
+
+export default Search;
