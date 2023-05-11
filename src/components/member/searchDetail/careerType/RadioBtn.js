e196cb8ea796d4d6e1443a843f0853b136a8c40d
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-11 14:28:41 +0900

    장애인쪽 상세 조건에 경력, 학력 추가

---
+++ src/components/member/searchDetail/careerType/RadioBtn.js
@@ -0,0 +1,19 @@
+const RadioBtn = ({ value, labelValue, defaultChecked }) => {
+  return (
+    <p className="p-4">
+      <input
+        type="radio"
+        id={value}
+        name="career-type"
+        value={value}
+        className="h-4 w-4 mb-1 border-gray-300 align-middle"
+        defaultChecked={defaultChecked}
+      />
+      <label htmlFor={value} className="pl-2">
+        {labelValue}
+      </label>
+    </p>
+  );
+};
+
+export default RadioBtn;
