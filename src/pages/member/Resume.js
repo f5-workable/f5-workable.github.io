4d34e7724f0cee3a5107001c5f8b5ee5f9f0473b
yelim1215 <yerim1215jg@naver.com>
2023-05-12 15:13:20 +0900

    이력서 목록 페이지의 이력서가 3개일 경우 추가하기 버튼 숨김 처리

--- src/pages/member/Resume.js
+++ src/pages/member/Resume.js
@@ -4,15 +4,6 @@ import Private from "../../components/member/resumeDetail/Private";
 import { BsFileEarmarkPlus, BsChevronRight } from "react-icons/bs";
 
 const Resume = () => {
-    // const [value, setValue] = useState("이력서 공개");
-
-    // const handleToggle = (event) => {
-    //     console.log(event.target.checked);
-    //     event.target.checked === false
-    //         ? setValue("이력서 공개")
-    //         : setValue("이력서 비공개")
-    // }
-
     const [resumeList, setResumeList] = useState([{}]);
 
     const handleClick = () => {
@@ -22,35 +13,6 @@ const Resume = () => {
     };
 
     return (
-      //   <div className="p-10 flex flex-col">
-      //   <h1 className="text-center text-3xl font-bold">이력서 목록</h1>
-      //   <div className=" px-60 py-10 flex justify-end">
-      //     <Private />
-      //   </div>
-      //   <div className="flex justify-center">
-      //     {resumeList.map((_, index) => (
-      //       <Link to={`/resume/${index}`} key={index}>
-      //           <div className="mx-5 bg-orange-100 w-48 h-64 rounded-md"></div>
-      //       </Link>
-      //     ))}
-      //   </div>
-      //   {resumeList.length < 3 ? (
-      //       <div className="py-10 flex items-center justify-center">
-      //           <button className="w-28 h-10 hover:bg-orange-300 border-2 flex items-center justify-center font-bold rounded-md" onClick={handleClick}>
-      //               <BsFileEarmarkPlus className="pr-1 text-2xl"/>
-      //               <span className="pl-1 ">추가하기</span>
-      //           </button>
-      //       </div>
-      //   ) : (
-      //       <div className="py-10 flex items-center justify-center">
-      //           <button className="w-28 h-10 border-2 flex items-center justify-center font-bold rounded-md opacity-50 cursor-not-allowed" disabled>
-      //               <BsFileEarmarkPlus className=" pr-1 text-2xl"/>
-      //               <span className="pl-1">추가하기</span>
-      //           </button>
-      //       </div>
-      //   )}
-      // </div>
-
       <div className="p-10 flex flex-col bg-gray-50 min-h-screen">
         <h1 className="text-center text-3xl font-bold mb-10">이력서 목록</h1>
         <div className="px-10 py-5 flex justify-end">
@@ -82,7 +44,7 @@ const Resume = () => {
               </button>
             </div>
           ) : (
-            <div className="bg-gray-300 rounded-lg shadow-lg flex items-center justify-center opacity-50 cursor-not-allowed">
+            <div className="bg-gray-300 rounded-lg shadow-lg flex items-center justify-center opacity-50 cursor-not-allowed hidden">
               <button
                 className="w-full h-10 bg-gray-300 text-gray-500 font-bold rounded-md flex items-center justify-center"
                 disabled
