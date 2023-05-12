7a25bbce5cfcffcdbcef86fac0f12d53609d7eb0
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-12 17:15:23 +0900

    장애인쪽 검색 상세조건의 라디오 버튼들을 모두 체크박스로 변경, 전체를 선택했을 때 다른 체크박스들 체크 해제, 모든 컴포넌트 state로 연결

--- src/components/member/searchDetail/companyType/index.js
+++ src/components/member/searchDetail/companyType/index.js
@@ -1,17 +1,48 @@
-import RadioBtn from "./RadioBtn";
+import Checkbox from "./Checkbox";
 
-const CompanyType = () => {
+const CompanyType = ({ setState }) => {
+  const changeCompanyType = (e) => {
+    const checkboxes = document.getElementsByName("company-type");
+    const targetValue = e.target.value;
+    const targetChecked = e.target.checked;
+
+    if (targetValue === "기업형태전체") {
+      checkboxes[0].checked = targetChecked;
+      checkboxes.forEach((checkbox, idx) => {
+        if (idx !== 0) {
+          checkbox.checked = false;
+        }
+      });
+      targetChecked
+        ? setState(["개인", "공사,공공", "대기업", "외국계", "중소", "협회,단체"])
+        : setState([]);
+    } else {
+      const arr = [];
+      checkboxes[0].checked = false;
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked === true) {
+          arr.push(checkbox.value);
+        }
+      });
+      setState(arr);
+    }
+  };
   return (
     <div className="mt-8">
       <p className="block mb-2 text-lg font-semibold">기업형태</p>
       <div className="flex items-center">
-        <RadioBtn value="전체" labelValue="전체" defaultChecked />
-        <RadioBtn value="개인" labelValue="개인" />
-        <RadioBtn value="공사,공공" labelValue="공사,공공" />
-        <RadioBtn value="대기업" labelValue="대기업" />
-        <RadioBtn value="외국계" labelValue="외국계" />
-        <RadioBtn value="중소" labelValue="중소" />
-        <RadioBtn value="협회,단체" labelValue="협회,단체" />
+        <Checkbox
+          value="기업형태전체"
+          labelValue="전체"
+          onChange={changeCompanyType}
+          defaultChecked
+        />
+        <Checkbox value="개인" labelValue="개인" onChange={changeCompanyType} />
+        <Checkbox value="공사,공공" labelValue="공사,공공" onChange={changeCompanyType} />
+        <Checkbox value="대기업" labelValue="대기업" onChange={changeCompanyType} />
+        <Checkbox value="외국계" labelValue="외국계" onChange={changeCompanyType} />
+        <Checkbox value="중소" labelValue="중소" onChange={changeCompanyType} />
+        <Checkbox value="협회,단체" labelValue="협회,단체" onChange={changeCompanyType} />
       </div>
     </div>
   );
--- src/components/member/searchDetail/paymentType/index.js
+++ src/components/member/searchDetail/paymentType/index.js
@@ -1,15 +1,39 @@
-import RadioBtn from "./RadioBtn";
+import Checkbox from "./Checkbox";
 
-const PaymentType = () => {
+const PaymentType = ({ setState }) => {
+  const changePaymentType = (e) => {
+    const checkboxes = document.getElementsByName("payment-type");
+    const targetValue = e.target.value;
+    const targetChecked = e.target.checked;
+
+    if (targetValue === "임금형태전체") {
+      checkboxes[0].checked = targetChecked;
+      checkboxes.forEach((checkbox, idx) => {
+        if (idx !== 0) {
+          checkbox.checked = false;
+        }
+      });
+      targetChecked ? setState(["시급", "일급", "월급", "연봉"]) : setState([]);
+    } else {
+      const arr = [];
+      checkboxes[0].checked = false;
+      checkboxes.forEach((checkbox) => {
+        if (checkbox.checked === true) {
+          arr.push(checkbox.value);
+        }
+      });
+      setState(arr);
+    }
+  };
   return (
     <div>
       <p className="block my-2 text-lg font-semibold mt-8">임금형태</p>
       <div className="flex items-center">
-        <RadioBtn value="전체" labelValue="전체" defaultChecked />
-        <RadioBtn value="시급" labelValue="시급" />
-        <RadioBtn value="일급" labelValue="일급" />
-        <RadioBtn value="월급" labelValue="월급" />
-        <RadioBtn value="연봉" labelValue="연봉" />
+        <Checkbox value="임금형태전체" labelValue="전체" onChange={changePaymentType} defaultChecked />
+        <Checkbox value="시급" labelValue="시급" onChange={changePaymentType} />
+        <Checkbox value="일급" labelValue="일급" onChange={changePaymentType} />
+        <Checkbox value="월급" labelValue="월급" onChange={changePaymentType} />
+        <Checkbox value="연봉" labelValue="연봉" onChange={changePaymentType} />
       </div>
     </div>
   );
