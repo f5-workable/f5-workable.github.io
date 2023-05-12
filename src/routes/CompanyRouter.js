178bc0a0c9036f153de07aab289ec9f4180d0fee
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-12 17:26:25 +0900

    라우터 설정 충돌 해결

--- src/routes/CompanyRouter.js
+++ src/routes/CompanyRouter.js
@@ -1,9 +1,9 @@
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Home from "../pages/company/Home";
 import Footer from "../components/Footer";
-import LoginCom from "../pages/auth/LoginCom";
-import SignUpCom from "../pages/auth/SignUpCom";
 import CompanyHeader from "../components/CompanyHeader";
+import Search from "../pages/company/Search";
+import ApplicantStatus from "../pages/company/ApplicantStatus";
 
 const CompanyRouter = ({ handleIsCompany }) => {
   return (
@@ -19,9 +19,8 @@ const CompanyRouter = ({ handleIsCompany }) => {
       >
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/" element={<Home />} />
-        <Route path="/logincom" element={<LoginCom />} />
-        <Route path="/signupcom" element={<SignUpCom />} />
-        <Route path="/jobs" element={<span>전체 구직 페이지</span>} />
+        <Route path="/status/applicant" element={<ApplicantStatus />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/jobs/:jobId" element={<span>상세페이지</span>} />
         <Route path="/profile" element={<span>프로필</span>} />
       </Route>
