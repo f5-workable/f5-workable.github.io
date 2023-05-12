e1a1e771730109246b9eaef65021f36742084e04
yelim1215 <yerim1215jg@naver.com>
2023-05-12 13:28:07 +0900

    기업 로고 사진 업로드 시 미리보기 구현

--- src/components/company/ProfileImg/index.js
+++ src/components/company/ProfileImg/index.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+
 const Profile = () => {
-    return (
-        <form method="post" enctype="multipart/form-data">
-            {/* <div class="button">
-                <label for="chooseFile">
-                    파일 업로드
-                </label>
-            </div> */}
-            <input type="file" id="chooseFile" name="chooseFile" accept="image/*" onChange="loadFile(this)" />
-        </form>
-    );
+  const [previewUrl, setPreviewUrl] = useState('');
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <form method="post" enctype="multipart/form-data">
+      <input
+        type="file"
+        id="chooseFile"
+        name="chooseFile"
+        accept="image/*"
+        onChange={handleFileInputChange}
+      />
+      {previewUrl && <img src={previewUrl} alt="Preview" />}
+    </form>
+  );
 };
 
 export default Profile;
