ca23dc93263e7f08cfbced48e910ed4d582e1d3a
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-14 12:36:05 +0900

    조건 초기화 기능 추가

--- src/pages/company/Search.js
+++ src/pages/company/Search.js
@@ -3,26 +3,26 @@ import SearchDetail from "../../components/company/searchDetail";
 import SearchedItems from "../../components/company/searchedItems";
 
 const Search = () => {
-  const [sortBy, setSortBy] = useState("");
+  const [sortBy, setSortBy] = useState("최신순");
   const ChangeSortBy = (e) => setSortBy(e.target.value);
 
   return (
     <main className="w-full h-full py-10">
-    <SearchDetail />
-    <div className="w-11/12 lg:w-3/4 h-auto mx-auto">
-      <div className="flex justify-between items-center mb-8">
-        <h2 className="text-2xl font-bold">검색결과 - 10개</h2>
-        <select
-          className="text-lg px-3 py-2 border border-solid border-neutral-300 rounded-lg text-neutral-600"
-          onChange={ChangeSortBy}
-        >
-          <option value="최신순">최신순</option>
-          <option value="임금순">임금순</option>
-        </select>
+      <SearchDetail />
+      <div className="w-11/12 lg:w-3/4 h-auto mx-auto">
+        <div className="flex justify-between items-center pb-8 mb-8 border-b border-solid border-neutral-300">
+          <h2 className="text-3xl font-bold text-neutral-700">검색결과 - 10건</h2>
+          <select
+            className="px-3 py-2 border border-solid border-neutral-300 rounded-lg text-lg text-neutral-600"
+            onChange={ChangeSortBy}
+          >
+            <option value="최신순">최신순</option>
+            <option value="임금순">희망임금순</option>
+          </select>
+        </div>
+        <SearchedItems sortBy={sortBy} />
       </div>
-      <SearchedItems sortBy={sortBy} />
-    </div>
-  </main>
+    </main>
   );
 };
 
