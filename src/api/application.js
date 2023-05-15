498487f0582da789669e43957d845de28bc012a8
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-15 16:24:59 +0900

    axios를 이용해 현재까지 완성된 API 연결

---
+++ src/api/application.js
@@ -0,0 +1,35 @@
+import { request } from ".";
+
+const application = {
+  /**
+   * 
+   * @param {*} j_id 구인공고 아이디
+   * @param {*} r_id 선택된 이력서 아이디
+   * @returns 
+   */
+  add: (j_id, r_id) => request.post("/apply", {}, { params: { j_id, r_id } }),
+
+  /**
+   * 
+   * @param {*} m_num 멤버 시퀀스 아이디
+   * @param {string} state 조회하는 지원 상태
+   * @returns 
+   */
+  retrieve: (m_num, state) => request.get(`/apply/list/${m_num}`, {}, { params: { state } }),
+
+  /**
+   * 
+   * @param {*} m_num 멤버 시퀀스 아이디
+   * @returns 
+   */
+  count: (m_num) => request.get(`/apply/list/count/${m_num}`),
+  
+  /**
+   * 
+   * @param {*} a_id 지원서 아이디
+   * @returns 
+   */
+  delete: (a_id) => request.delete(`/apply/${a_id}`),
+};
+
+export default application;
