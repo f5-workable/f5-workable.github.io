eea2bec8dfc87b359fd8123a160ba389f70235c9
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-16 16:42:37 +0900

    검색결과가 없을 때 나타나는 문구 추가

--- src/components/member/searchedItems/index.js
+++ src/components/member/searchedItems/index.js
@@ -1,7 +1,9 @@
 import Item from "./Item";
 
-const SearchedItems = () => {
-  return (
+const SearchedItems = ({ boards }) => {
+  return boards?.length === 0 ? (
+    <p className="text-xl text-neutral-400 text-center mt-40">기업이 존재하지 않습니다.</p>
+  ) : (
     <div className="w-full h-auto grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 pb-10">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, idx) => (
         <Item idx={idx} key={idx} />
