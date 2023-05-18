c9ac41b244825f96b25366d3a2bb21658b457557
yelim1215 <yerim1215jg@naver.com>
2023-05-18 14:27:15 +0900

    이력서 작성 통신

--- src/components/member/resumeDetail/Disorder/SubDisorder.js
+++ src/components/member/resumeDetail/Disorder/SubDisorder.js
@@ -1,7 +1,7 @@
-const SubDisorder = ({value}) => {
+const SubDisorder = ({value, setState}) => {
     return (
         <div>
-            <input type="checkbox" id={value} value={value} />
+            <input type="checkbox" id={value} value={value} onClick={ () => setState(value) } />
             <label htmlFor={value} className="px-2">{value}</label>
         </div>
     );
--- src/components/member/resumeDetail/Disorder/index.js
+++ src/components/member/resumeDetail/Disorder/index.js
@@ -1,24 +1,24 @@
 import SubDisorder from "./SubDisorder";
 
-const Disorder = () => {
+const Disorder = ({setState}) => {
     return(
         <span className=" pt-2 grid grid-cols-5 gap-4">
-            <SubDisorder value="간장애" />
-            <SubDisorder value="뇌병변장애" />
-            <SubDisorder value="뇌전증장애" />
-            <SubDisorder value="시각장애" />
-            <SubDisorder value="신장장애" />
-            <SubDisorder value="심장장애" />
-            <SubDisorder value="안면장애" />
-            <SubDisorder value="언어장애" />
-            <SubDisorder value="자폐성장애" />
-            <SubDisorder value="장루요루장애" />
-            <SubDisorder value="정신장애" />
-            <SubDisorder value="지적장애" />
-            <SubDisorder value="지체장애" />
-            <SubDisorder value="호흡기장애" />
-            <SubDisorder value="특수교육법상장애인" />
-            <SubDisorder value="국가유공" />
+            <SubDisorder value="간장애" setState={setState} />
+            <SubDisorder value="뇌병변장애" setState={setState} />
+            <SubDisorder value="뇌전증장애" setState={setState} />
+            <SubDisorder value="시각장애" setState={setState} />
+            <SubDisorder value="신장장애" setState={setState} />
+            <SubDisorder value="심장장애" setState={setState} />
+            <SubDisorder value="안면장애" setState={setState} />
+            <SubDisorder value="언어장애" setState={setState} />
+            <SubDisorder value="자폐성장애" setState={setState} />
+            <SubDisorder value="장루요루장애" setState={setState} />
+            <SubDisorder value="정신장애" setState={setState} />
+            <SubDisorder value="지적장애" setState={setState} />
+            <SubDisorder value="지체장애" setState={setState} />
+            <SubDisorder value="호흡기장애" setState={setState} />
+            <SubDisorder value="특수교육법상장애인" setState={setState} />
+            <SubDisorder value="국가유공" setState={setState} />
         </span>
     );
 };
