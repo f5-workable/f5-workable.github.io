cf328e1d36d67e19a80000c9708183aa0678c724
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-24 21:43:16 +0900

    회원별 북마크한 공고를 가져오도록 구현

--- src/pages/member/Bookmark.js
+++ src/pages/member/Bookmark.js
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
+import api from "../../api";
 import SearchedItems from "../../components/member/searchedItems";
+import { useState } from "react";
 
 const Bookmark = () => {
+  const [boards, setBoards] = useState([]);
+  const getBookmarks = async () => {
+    const { data } = await api.bookmark.retrieveByMemberId(1);
+    setBoards(data.list)
+  };
+
+  useEffect(() => {
+    getBookmarks();
+  }, []);
+
   return (
     <main className="w-full h-full pt-14 pb-10 ">
       <div className="w-11/12 lg:w-3/4 h-auto min-h-full mx-auto">
         <div className="mb-8">
           <h2 className="text-2xl font-semibold text-neutral-800">북마크</h2>
         </div>
-        <SearchedItems />
+        <SearchedItems boards={boards} />
       </div>
     </main>
   );
