3924a7425d2f11de560ccfe6032a77b0d7ff0945
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-25 03:18:59 +0900

    Link 경로 수정

--- src/components/company/searchedItems/Item.js
+++ src/components/company/searchedItems/Item.js
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
-const Item = ({ id, applicant }) => {
+const Item = ({ resume }) => {
   return (
-    <Link to={`/resume/${id}`} className="block w-full h-80">
+    <Link to={`/resume/${resume.r_id}`} className="block w-full h-80">
       <div className="flex justify-center items-center w-full h-3/5 rounded-md overflow-hidden shadow-md">
         <img
           src="https://www.cvpartners.co.kr/Content/images/house/1/5.png"
@@ -11,9 +11,9 @@ const Item = ({ id, applicant }) => {
         />
       </div>
       <div className="w-full h-2/5 relative">
-        <p className="py-2 text-lg font-bold">이름</p>
-        <p className="text-neutral-500">장애유형 | 중증여부</p>
-        <p className="py-2 text-neutral-500">희망직종</p>
+        <p className="pt-2 text-lg font-bold">이름</p>
+        <p className="pt-1 text-neutral-500 font-medium truncate break-all">장애유형 | 중증여부</p>
+        <p className="py-2 text-neutral-500 truncate break-all">희망직종</p>
         <p className="mt-5 font-semibold absolute bottom-2">희망 임금 (원)</p>
       </div>
     </Link>
