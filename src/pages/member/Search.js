6f8d9844142b05e19d5860ccd296c4596607e007
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-24 21:50:30 +0900

    뒤로가기 시에 검색했던 데이터 및 위치 유지, 정렬 state 검색 파라미터에 포함

--- src/pages/member/Search.js
+++ src/pages/member/Search.js
@@ -3,9 +3,12 @@ import SearchDetail from "../../components/member/searchDetail";
 import { useState } from "react";
 import api from "../../api";
 import { MdSearch } from "react-icons/md";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const Search = () => {
+  const history = useLocation();
+  const navigate = useNavigate();
   const [isArrowBtnClicked, setIsArrowBtnClicked] = useState(false);
   const [sortBy, setSortBy] = useState("최신순");
   const [careerType, setCareerType] = useState([]);
@@ -16,8 +19,9 @@ const Search = () => {
   const [paymentType, setPaymentType] = useState([]);
   const [keyword, setKeyword] = useState("");
   const [keywordType, setKeywordType] = useState("기업명");
+  const [searchedTotal, setSearchedTotal] = useState(0);
   const [boards, setBoards] = useState(
-    /** @type {import("../../api/companyBoard").BoardList} */ ({})
+    /** @type {import("../../api/companyBoard").BoardList} */ ([])
   );
 
   const hiddenDropdown = () => {
@@ -28,91 +32,94 @@ const Search = () => {
 
   const searchCompanyBoard = async (e) => {
     e.preventDefault();
-    const { data } = await api.companyBoard.search(
+    const {
+      data: { list, total },
+    } = await api.companyBoard.search(
       employmentType,
       paymentType,
       location,
       companyType,
       keywordType,
-      keyword
+      keyword,
+      sortBy
     );
     hiddenDropdown();
-    setBoards(data);
+    setSearchedTotal(total);
+    setBoards(list);
+    navigate("/search", {
+      state: { boards: list, searchedTotal: total, scrollY: window.scrollY },
+    });
   };
 
   useEffect(() => {
-    if (sortBy === "최신순")
-      boards.list?.sort((a, b) => {
-        return new Date(b.d_date) - new Date(a.d_date);
-      });
-  }, [boards, sortBy]);
+    if (history.state) {
+      window.scrollTo({ top: history.state.scrollY, behavior: "smooth" });
+    }
+  }, [history.state]);
 
   return (
     <main className="w-full h-full py-10">
-      <form
-        onSubmit={searchCompanyBoard}
-        className="mb-20 w-5/6 max-w-4xl mx-auto bg-neutral-50 p-10 rounded-2xl border-2 border-solid border-neutral-100"
-      >
-        <div className="relative mb-4 overflow-hidden">
-          <select
-            name="keyword-type"
-            className="absolute top-1/2 -translate-y-1/2 left-3 p-3 bg-transparent outline-none text-lg"
-            value={keywordType}
-            onChange={(e) => setKeywordType(e.target.value)}
-          >
-            <option value="기업명">기업명</option>
-            <option value="업종">업종</option>
-          </select>
-          <input
-            id="keyword"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
-            className="w-full pl-28 px-4 py-4 rounded-3xl border-2 outline-2 focus:outline-indigo-300 text-lg"
-            placeholder="검색어를 입력해주세요."
-          />
-          <button className="w-1/12 py-1 bg-transparent text-neutral-500 absolute top-3 right-2 flex justify-center">
-            <MdSearch fontSize={28} />
-          </button>
-        </div>
-        <SearchDetail
-          state={{
-            careerType,
-            companyType,
-            educationType,
-            employmentType,
-            location,
-            paymentType,
-            isArrowBtnClicked,
-          }}
-          setState={{
-            setCareerType,
-            setCompanyType,
-            setEducationType,
-            setEmploymentType,
-            setLocation,
-            setPaymentType,
-            setIsArrowBtnClicked,
-          }}
-        />
-      </form>
-      {boards.list?.length > 0 && (
-        <div className="w-11/12 lg:w-3/4 h-auto mx-auto">
-          <div className="flex justify-between items-center pb-8 mb-8 border-b border-solid border-neutral-300">
-            <h2 className="text-3xl font-bold text-neutral-700">
-              검색결과 - {boards.total}건
-            </h2>
+      <form onSubmit={searchCompanyBoard} className="w-11/12 lg:w-3/4 h-auto mx-auto">
+        <div className="mb-20 w-5/6 max-w-4xl mx-auto bg-neutral-50 p-10 rounded-2xl border-2 border-solid border-neutral-100">
+          <div className="relative mb-4 overflow-hidden">
             <select
-              className="px-3 py-2 border border-solid border-neutral-300 rounded-lg text-lg text-neutral-600"
-              onChange={(e) => setSortBy(e.target.value)}
+              name="keyword-type"
+              className="absolute top-1/2 -translate-y-1/2 left-3 p-3 bg-transparent outline-none text-lg"
+              value={keywordType}
+              onChange={(e) => setKeywordType(e.target.value)}
             >
-              <option value="최신순">최신순</option>
-              <option value="임금순">임금순</option>
-              <option value="마감일순">마감일순</option>
+              <option value="기업명">기업명</option>
+              <option value="업종">업종</option>
             </select>
+            <input
+              id="keyword"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+              className="w-full pl-28 px-4 py-4 rounded-3xl border-2 outline-2 focus:outline-indigo-300 text-lg"
+              placeholder="검색어를 입력해주세요."
+            />
+            <button className="w-1/12 py-1 bg-transparent text-neutral-500 absolute top-3 right-2 flex justify-center">
+              <MdSearch fontSize={28} />
+            </button>
           </div>
-          <SearchedItems boards={boards.list} />
+          <SearchDetail
+            state={{
+              careerType,
+              companyType,
+              educationType,
+              employmentType,
+              location,
+              paymentType,
+              isArrowBtnClicked,
+            }}
+            setState={{
+              setCareerType,
+              setCompanyType,
+              setEducationType,
+              setEmploymentType,
+              setLocation,
+              setPaymentType,
+              setIsArrowBtnClicked,
+            }}
+          />
         </div>
-      )}
+        <div className="flex justify-between items-center pb-8 mb-8 border-b border-solid border-neutral-300">
+          <h2 className="text-3xl font-bold text-neutral-700">
+            검색결과 - {history.state?.searchedTotal || searchedTotal}건
+          </h2>
+          <select
+            className="px-3 py-2 border border-solid border-neutral-300 rounded-lg text-lg text-neutral-600"
+            onChange={(e) => setSortBy(e.target.value)}
+          >
+            <option value="최신순">최신순</option>
+            <option value="임금순">임금순</option>
+            <option value="마감일순">마감일순</option>
+          </select>
+        </div>
+      </form>
+      <div className="w-11/12 lg:w-3/4 h-auto mx-auto">
+        <SearchedItems boards={history.state?.boards || boards} />
+      </div>
     </main>
   );
 };
