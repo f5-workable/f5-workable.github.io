b4d7678aa591244b55caf43629da29b7375dc3c5
yelim1215 <yerim1215jg@naver.com>
2023-05-26 16:30:42 +0900

    개인용 회원정보 조회 페이지 데이터 불러오기 성공

--- src/components/member/userDetail/UserDetailList.js
+++ src/components/member/userDetail/UserDetailList.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import api from "../../../api";
+
 const UserDetailList = () => {
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+
+    const getMember = async () => {
+        const memberId = localStorage.getItem("memberId") || sessionStorage.getItem("memberId");
+        const { data } = await api.member.retrieve(memberId);
+        console.log(memberId);
+        console.log(data);
+        setName(data.name);
+        setId(data.id);
+        setPhone(data.phone);
+        setEmail(data.email);
+    };
+
+    useEffect(() => {
+        getMember();
+    }, []);
+
     return (
         <div className="mx-16 py-16 w-3/5 h-auto">
             <div className="block text-center text-3xl font-bold">회원정보</div>
@@ -6,25 +29,25 @@ const UserDetailList = () => {
                 <div className=" h-32 mt-10">
                     <label className=" text-xl font-bold px-10">이름</label>
                     <hr className=" bg-black h-0.5"></hr>
-                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={"이예림"} disabled />
+                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={id} disabled />
                     <hr></hr>
                 </div>
                 <div className=" h-32">
                     <label className=" text-xl font-bold px-10">아이디</label>
                     <hr className=" bg-black h-0.5"></hr>
-                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={"yelim1215"} disabled />
+                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={name} disabled />
                     <hr></hr>
                 </div>
                 <div className=" h-32">
                     <label className=" text-xl font-bold px-10">연락처</label>
                     <hr className=" bg-black h-0.5"></hr>
-                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={"010-3047-5988"} disabled />
+                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={phone} disabled />
                     <hr></hr>
                 </div>
                 <div className=" h-32">
                     <label className=" text-xl font-bold px-10">이메일</label>
                     <hr className=" bg-black h-0.5"></hr>
-                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={"yerim1215jg@naver.com"} disabled />
+                    <input className=" text-lg font-bold italic bg-white my-5 px-10" value={email} disabled />
                     <hr></hr>
                 </div>
             </div>
