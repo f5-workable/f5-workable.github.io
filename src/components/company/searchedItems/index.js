4a169dd6635f3728f38cb7c897d1e3be3796172d
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-30 18:15:36 +0900

    데이터가 없으면서 API 로딩 중이 아닐 때만 존재하지 않는다는 메시지를 보여주도록 변경

--- src/components/company/searchedItems/index.js
+++ src/components/company/searchedItems/index.js
@@ -1,24 +1,28 @@
 import Item from "./Item";
 
-const SearchedItems = ({ resumes, applicants }) => {
+const SearchedItems = ({ resumes, applicants, isLoading }) => {
   return applicants ? (
-    applicants?.length === 0 ? (
+    applicants?.length === 0 && !isLoading ? (
       <p className="text-xl text-neutral-400 text-center mt-40">지원자가 존재하지 않습니다.</p>
     ) : (
-      <div className="w-full h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 pb-10">
-        {applicants?.map((resume) => (
-        <Item key={resume.cr_num} resume={resume} />
-      ))}
-      </div>
+      applicants?.length > 0 && (
+        <div className="w-full h-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 pb-10">
+          {applicants?.map((applicant) => (
+            <Item key={applicant.cr_num} applicant={applicant} />
+          ))}
+        </div>
+      )
     )
-  ) : resumes?.length === 0 ? (
+  ) : resumes?.length === 0 && !isLoading ? (
     <p className="text-xl text-neutral-400 text-center mt-40">검색결과가 존재하지 않습니다.</p>
   ) : (
-    <div className="w-full h-auto grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 pb-10">
-       {resumes?.map((resume) => (
-        <Item key={resume.cr_num} resume={resume} />
-      ))}
-    </div>
+    resumes?.length > 0 && (
+      <div className="w-full h-auto grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 pb-10">
+        {resumes?.map((resume) => (
+          <Item key={resume.r_id} resume={resume} />
+        ))}
+      </div>
+    )
   );
 };
 
