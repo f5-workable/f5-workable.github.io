c2b159466b11917035cbe671069f35c214be1d5f
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-30 18:24:39 +0900

    최근 본 지원자 및 우리 기업 공고 페이지를 프로필 사이드바에 링크 추가

--- src/pages/company/Home.js
+++ src/pages/company/Home.js
@@ -4,9 +4,13 @@ import CustomCompanySlider from "../../components/company/slider/company";
 import CustomMemberSlider from "../../components/company/slider/member";
 import { useEffect, useState } from "react";
 import api from "../../api";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const [companyBoards, setCompanyBoards] = useState();
+  const recentViewedResumes = JSON.parse(localStorage.getItem("recentViewedResume"));
+  recentViewedResumes?.reverse();
 
   const getBoardsByCompanyId = async () => {
     const companyId = localStorage.getItem("companyId") || sessionStorage.getItem("companyId");
@@ -24,34 +28,24 @@ function Home() {
         <HeaderSlider />
       </section>
       <section className="w-5/6 lg:w-3/4 mb-20 mx-auto">
-        <div className="flex items-center border-b border-solid pb-3 border-neutral-500">
+        <Link to="/company/boards" className="flex items-center border-b border-solid pb-3 border-neutral-500">
           <h2 className="text-2xl font-medium inline mr-5">우리 기업 공고</h2>
           <MdAdd
             className="inline border border-solid border-orange-500 text-orange-500 rounded-full"
             fontSize={30}
           />
-        </div>
+        </Link>
         <CustomCompanySlider boards={companyBoards} />
       </section>
       <section className="w-5/6 lg:w-3/4 mb-20 mx-auto">
-        <div className="flex items-center border-b border-solid pb-3 border-neutral-500">
-          <h2 className="text-2xl font-medium inline mr-5">새로운 지원자</h2>
-          <MdAdd
-            className="inline border border-solid border-orange-500 text-orange-500 rounded-full"
-            fontSize={30}
-          />
-        </div>
-        <CustomMemberSlider />
-      </section>
-      <section className="w-5/6 lg:w-3/4 mb-20 mx-auto">
-        <div className="flex items-center border-b border-solid pb-3 border-neutral-500">
+        <Link to="/recent/applicants" className="flex items-center border-b border-solid pb-3 border-neutral-500">
           <h2 className="text-2xl font-medium inline mr-5">최근 본 지원자</h2>
           <MdAdd
             className="inline border border-solid border-orange-500 text-orange-500 rounded-full"
             fontSize={30}
           />
-        </div>
-        <CustomMemberSlider />
+        </Link>
+        <CustomMemberSlider resumes={recentViewedResumes} />
       </section>
     </main>
   );
