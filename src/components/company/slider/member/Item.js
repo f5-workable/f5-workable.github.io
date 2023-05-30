1edb816de48aee83e7f7c0f9ba94439f18848817
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-30 18:18:06 +0900

    이력서마다 회원 정보를 불러오도록 변경

--- src/components/company/slider/member/Item.js
+++ src/components/company/slider/member/Item.js
@@ -1,8 +1,21 @@
+import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import api from "../../../../api";
 
-const Item = ({ key }) => {
+const Item = ({ resume }) => {
+  const [memberInfo, setMemberInfo] = useState();
+
+  const getMemberInfo = useCallback(async () => {
+    const info = await api.member.retrieve(resume.m_num);
+    setMemberInfo(info);
+  }, [resume]);
+
+  useEffect(() => {
+    getMemberInfo();
+  }, [getMemberInfo]);
+
   return (
-    <Link to={`/resume/${key}`} key={key} className="block w-full h-80">
+    <Link to={`/resume/${resume.r_id}`} className="block w-full h-80">
       <div className="w-full h-full">
         <div className="flex justify-center items-center w-full h-3/5 rounded-md overflow-hidden shadow-md">
           <img
@@ -12,10 +25,14 @@ const Item = ({ key }) => {
           />
         </div>
         <div className="w-full h-2/5 relative">
-          <p className="py-2 text-lg font-bold">이름</p>
-          <p className="text-neutral-500">장애유형 | 중증여부</p>
-          <p className="py-2 text-neutral-500">희망직종</p>
-          <p className="mt-5 font-semibold absolute bottom-2">희망 임금 (원)</p>
+          <p className="py-2 text-lg font-bold">{memberInfo?.name || "이름없음"}</p>
+          <p className="text-neutral-500">
+            {resume.ob_type} | {resume.disease}
+          </p>
+          <p className="py-2 text-neutral-500">{resume.job}</p>
+          <p className="mt-5 font-semibold absolute bottom-2">
+            희망{resume.payment_type} {resume.payment?.toLocaleString()}원
+          </p>
         </div>
       </div>
     </Link>
