72a70e835df83ab92dcb4d014a873e920f7997f6
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-05-30 18:30:15 +0900

    들여쓰기 수정

--- src/pages/member/UserDetail.js
+++ src/pages/member/UserDetail.js
@@ -1,13 +1,13 @@
 import UserDetailList from "../../components/member/userDetail/UserDetailList";
 import UserDetailBar from "../../components/member/userDetail/UserDetailBar";
 
-const UserDetail = () => {
-    return (
-        <div className=" mx-24 flex justify-center">
-            <UserDetailBar />
-            <UserDetailList />
-        </div>
-    );
+const UserDetail = ({ setIsLogined }) => {
+  return (
+    <div className=" mx-24 flex justify-center">
+      <UserDetailBar setIsLogined={setIsLogined} />
+      <UserDetailList />
+    </div>
+  );
 };
 
 export default UserDetail;
